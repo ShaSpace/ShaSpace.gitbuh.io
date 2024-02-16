2fd9c959eb8fde58871864d88932c9ba2380c20e
ShaSpace <shalynn.clarke@gmail.com>
2024-02-16 12:51:07 -0500

    Contact Page Initiation + Video Placeholders

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,21 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
+// Moving through the carousel with the mouse wheel.
+
+// Invokes the Carousel
+$('#myCarousel').carousel({interval:3000});
+
+$('#myCarousel').bind('mousewheel', function(e)
+{
+    if(e.originalEvent.wheelDelta /120 > 0)
+    {
+        $(this).carousel('next');
+    }
+    else
+    {
+        $(this).carousel('prev');
+    }
+});
+
